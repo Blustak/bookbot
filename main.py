ec80d1b73dc94aaffb814443dae207f0e6381c44
Blustak <menditta1@gmail.com>
2024-12-19 19:55:57 +0000

    finished word count func

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 def main():
     with open("./books/frankenstein.txt") as f:
         file_contents = f.read()
-        print(file_contents)
+        word_count = count_words(file_contents)
+        print(f"Word count:{word_count}")
+
+
+def count_words(s: str):
+    return len(s.split())
 
 
 if __name__ == "__main__":
