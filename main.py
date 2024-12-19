60eaabc5c4c558324626fe917314df683a45c8de
Blustak <menditta1@gmail.com>
2024-12-19 20:18:14 +0000

    finalised generate_report function

--- main.py
+++ main.py
@@ -1,12 +1,40 @@
 def main():
-    with open("./books/frankenstein.txt") as f:
+    book_path = "./books/frankenstein.txt"
+    with open(book_path) as f:
         file_contents = f.read()
         word_count = count_words(file_contents)
-        print(f"Word count:{word_count}")
+        character_count = character_occurences(file_contents)
+        generate_report(book_path, word_count, character_count)
 
 
 def count_words(s: str):
     return len(s.split())
+
+
+def character_occurences(s: str):
+    s = s.lower()
+    unique_chars = set(filter(lambda x: x.isalpha(), s))
+    occurences = {}
+    for c in unique_chars:
+        occurences[c] = s.count(c)
+    return occurences
+
+
+def generate_report(book_path, word_count, letter_occurences):
+    letter_occurences = list(
+        map(lambda x: (x, letter_occurences[x]), letter_occurences)
+    )
+    letter_occurences.sort(reverse=True, key=lambda x: x[1])
+    print(
+        f"""
+    ---BOOK REPORT:{book_path}---
+    There were {word_count} words found in the document
+    ***
+    """
+    )
+    for letter, count in letter_occurences:
+        print(f"The {letter} character was found {count} times.")
+    print("---END OF REPORT---")
 
 
 if __name__ == "__main__":
